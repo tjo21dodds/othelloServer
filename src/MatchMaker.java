b24c484bb16a8306d1b5283d6db05e46d7b45596
Thomas Dodds <tjo21doddshome@gmail.com>
2021-06-16 02:24:53 +0100

    Finished off client host matching

--- src/MatchMaker.java
+++ src/MatchMaker.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class MatchMaker {
 
-    final int serverPort =1700;
+    final int serverPort = 1700;
 
 
     AtomicInteger nextPort = new AtomicInteger(1701);
@@ -18,7 +18,7 @@ public class MatchMaker {
     ServerSocket serverSocket;
     private ArrayList<MatchHandler> hosts = new ArrayList<>(); //List of sockets waiting for a game.
 
-    private void handleHost(Socket socket) throws IOException{
+    private void handleHost(Socket socket) throws IOException {
         // Inform host of new room.
         // : Composing Message
         Integer port = nextPort.incrementAndGet();
@@ -30,10 +30,10 @@ public class MatchMaker {
 
     }
 
-    public void handleClient(Socket socket) throws IOException{
+    public void handleClient(Socket socket) throws IOException {
         // Send list of hosts
         String msg = "ROOMS";
-        for (Iterator it = hosts.iterator(); it.hasNext();){
+        for (Iterator it = hosts.iterator(); it.hasNext(); ) {
             MatchHandler matchHandler = (MatchHandler) it.next();
             msg = msg + " ";
             msg = msg + String.valueOf(matchHandler.serverSocket.getLocalPort());
@@ -41,12 +41,13 @@ public class MatchMaker {
         NetworkHelper.write(msg, socket.getOutputStream());
 
     }
+
     public void handleServer() throws IOException {
         ServerSocket serverSocket = new ServerSocket(serverPort);
-        while (!serverSocket.isClosed()){
+        while (!serverSocket.isClosed()) {
 
             Socket socket = serverSocket.accept();
-            Thread thread = new Thread(()-> {
+            Thread thread = new Thread(() -> {
                 try {
                     String msg = NetworkHelper.msgRead(socket.getInputStream());
                     String[] args = msg.split(" ");
@@ -61,13 +62,17 @@ public class MatchMaker {
                             System.out.println("Handle server malformed message");
                             break;
                     }
-                }
-                catch (IOException ioException){
+                } catch (IOException ioException) {
                     ioException.printStackTrace();
                 }
             });
             thread.start();
 
         }
+    }
+
+    public static void main(String[] args) throws IOException{
+        MatchMaker matchMaker = new MatchMaker();
+        matchMaker.handleServer();
     }
 }
